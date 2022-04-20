6e2a0f707dc881dfc3bc3ca521b9ee0fde17e386
bobosyang <resurgence_yang@163.com>
2022-04-20 10:17:59 +0800

    Revise Apr19

--- src/main/java/com/leetcode/y2022/m04/Apr19.java
+++ src/main/java/com/leetcode/y2022/m04/Apr19.java
@@ -31,6 +31,103 @@ public class Apr19 {
 	
 	*/
 
+	/**
+	 * 参考1
+	 * 
+	 * @param s
+	 * @param c
+	 * @return
+	 */
+	public int[] shortestToChar1(String s, char c) {
+		char ch[] = s.toCharArray();
+		int ans[] = new int[ch.length];
+		Arrays.fill(ans, ch.length + 5);
+		for (int i = 0; i < ch.length; i++) {
+			int j = i;
+			while (j >= 0 && ch[j] != c) {
+				j--;
+			}
+			if (j >= 0) {
+				ans[i] = i - j;
+			}
+			j = i;
+			while (j < ch.length && ch[j] != c) {
+				j++;
+			}
+			if (j < ch.length) {
+				ans[i] = Math.min(ans[i], j - i);
+			}
+		}
+		return ans;
+	}
+
+	/**
+	 * 参考2
+	 * 
+	 * @param s
+	 * @param c
+	 * @return
+	 */
+	public int[] shortestToChar2(String s, char c) {
+		int idx[] = new int[10005];
+		idx[0] = -10005;
+		int p = 1;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == c) {
+				idx[p] = i;
+				p++;
+			}
+		}
+		idx[p] = 20010;
+		int[] ans = new int[s.length()];
+		for (int i = 0; i < ans.length; i++) {
+			int l = 1, r = p;
+			while (l < r) {
+				int mid = (l + r) >> 1;
+				if (idx[mid] < i) {
+					l = mid + 1;
+				} else {
+					r = mid;
+				}
+				if (l == r - 1) {
+					if (idx[l] >= i) {
+						r = l;
+					}
+					break;
+				}
+			}
+			ans[i] = Math.min(idx[r] - i, i - idx[r - 1]);
+		}
+		return ans;
+	}
+
+	/**
+	 * 参考3
+	 * 
+	 * @param s
+	 * @param c
+	 * @return
+	 */
+	public int[] shortestToChar3(String s, char c) {
+		char ch[] = s.toCharArray();
+		int ans[] = new int[ch.length];
+		int idx = -10005;
+		for (int i = 0; i < ch.length; i++) {
+			if (ch[i] == c) {
+				idx = i;
+			}
+			ans[i] = i - idx;
+		}
+		idx = 20010;
+		for (int i = ch.length - 1; i >= 0; i--) {
+			if (ch[i] == c) {
+				idx = i;
+			}
+			ans[i] = Math.min(ans[i], idx - i);
+		}
+		return ans;
+	}
+
 	public int[] shortestToChar(String s, char c) {
 		int[] shortestToTarget = new int[s.length()];
 		List<Integer> targetIndexes = new ArrayList<Integer>();
